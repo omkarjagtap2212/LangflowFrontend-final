81cb6b1de649623be09b151e1f50a24a1730f625
Omkar <omkarjagtap2312@gmail.com>
2025-01-05 03:07:39 +0530

    chnages .env and vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    outDir: 'build', // Ensure the build output is directed to the 'build' directory
+  },
   server: {
-    build: {
-      outDir: 'build', // Change this if Render expects a different directory
-    },
     proxy: {
       '/runFlow': 'http://localhost:5000',
     },
